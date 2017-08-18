54643ece7aa5abe822f2488cdc73a6be6e911b5e
Will <wmitbrodt@gmail.com>
2017-08-17 17:37:19 -0700

    Main routes set up 🛣

--- src/App.js
+++ src/App.js
@@ -1,18 +1,76 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
-import Nav from './components/Nav';
-import Home from './components/Home';
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Work from './components/Work'
+import Projects from './components/Projects'
 
-
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <Nav />
-        <Home />
-      </div>
-    );
+// Each logical "route" has two components, one for
+// the sidebar and one for the main area. We want to
+// render both of them in different places when the
+// path matches the current URL.
+const routes = [
+  { path: '/',
+    exact: true,
+    sidebar: () => <div>home!</div>,
+    main: Home
+  },
+  { path: '/about',
+    sidebar: () => <div>about</div>,
+    main: About
+  },
+  { path: '/work',
+    sidebar: () => <div>work</div>,
+    main: Work
+  },
+  { path: '/projects',
+    sidebar: () => <div>projects</div>,
+    main: Projects
   }
-}
+]
 
-export default App;
+const SideNav = () => (
+  <Router>
+    <div style={{ display: 'flex' }}>
+      <div id="nav">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="about">About</Link></li>
+          <li><Link to="/work">Work</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+        </ul>
+
+        {routes.map((route, index) => (
+          // You can render a <Route> in as many places
+          // as you want in your app. It will render along
+          // with any other <Route>s that also match the URL.
+          // So, a sidebar or breadcrumbs or anything else
+          // that requires you to render multiple things
+          // in multiple places at the same URL is nothing
+          // more than multiple <Route>s.
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.sidebar}
+          />
+        ))}
+      </div>
+
+      <div>
+        {routes.map((route, index) => (
+          // Render more <Route>s with the same paths as
+          // above, but different components this time.
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.main}
+          />
+        ))}
+      </div>
+    </div>
+  </Router>
+)
+
+export default SideNav
---
+++ src/components/Work.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Work extends Component {
+  render() {
+    return (
+      <div className="about">
+         <h1>Work</h1>
+      </div>
+    );
+  }
+}
+
+export default Work;
