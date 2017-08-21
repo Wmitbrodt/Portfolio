64ec778619c1246fe251090a5d02ff839b9b5625
Will <wmitbrodt@gmail.com>
2017-08-20 18:27:13 -0700

    something going

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,30 +14,22 @@ class Home extends Component {
     return (
       <div id="home">
         <section className="home__hero">
-          <div className="container__hero">
-
-            <h1 className="hero__will">Will Mitbrodt</h1>
-            <p>
-               Bicycle rights locavore woke waistcoat meditation. Readymade yr gluten-free taiyaki cold-pressed, cray literally chambray plaid.
-               Photo booth chambray kinfolk, subway tile next level locavore taxidermy gastropub humblebrag VHS shaman +1 normcore synth.
-               Hell of occupy biodiesel godard leggings offal organic helvetica adaptogen you probably haven't heard of them yr typewriter master cleanse slow-carb.
-               Offal messenger bag VHS everyday carry food truck. Forage salvia keffiyeh fixie health goth green juice franzen four dollar toast ennui.
-               Salvia single-origin coffee hot chicken, chambray shabby chic tote bag kombucha chillwave franzen flexitarian la croix swag tousled coloring book scenester.
-               Gastropub pitchfork mlkshk, tumeric seitan shabby chic copper mug readymade narwhal freegan.
-            </p>
+          <div className="hero__container">
+            <h1>Hi, I'm <span>Will</span>.</h1>
+            <h2> - I'm a web developer with a love of art, design &amp; psychology.</h2>
+            <a href="mailto:wmitbrodt@gmail.com"><h3>Available for freelance work →</h3></a>
           </div>
-          <ul className="particles">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-          <div class="element"></div>
+            <ul className="particles">
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
         </section>
         <section className="home__section__2">
 
-            <h1>Will Mitbrodt</h1>
+            <h1>Designer/Developer</h1>
             <p>
                Bicycle rights locavore woke waistcoat meditation. Readymade yr gluten-free taiyaki cold-pressed, cray literally chambray plaid.
                Photo booth chambray kinfolk, subway tile next level locavore taxidermy gastropub humblebrag VHS shaman +1 normcore synth.
