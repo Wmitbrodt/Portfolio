935758a72284b32380eaec1bb249b2401c11b04f
Will <wmitbrodt@gmail.com>
2017-08-30 23:11:31 -0700

    fixed spacing on about

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,12 @@ class About extends Component {
     return (
       <div id="about__container">
         <div>
-          <h1>About me.</h1>
+          <h1>About me</h1>
         </div>
         <div>
           <h3>I love tackling new problems in different ways, by combining my passion for art, code, psychology and technology.</h3>
           <div>
-            <a href="https://s3-us-west-2.amazonaws.com/portfolio-will/documents/will_mitbrodt_cv.pdf" target="_blank"><span>Download CV →</span></a>
+            <a target="_blank" href="https://s3-us-west-2.amazonaws.com/portfolio-will/documents/will_mitbrodt_cv.pdf" rel="noopener noreferrer"><span>Download CV →</span></a>
             <br /><br />
           </div>
         </div>
@@ -20,8 +20,8 @@ class About extends Component {
         <div>
           <h3>
             I'm Will, a Full-Stack Web Developer with a passion for Design.
-            I am a graduate of the<a target="_blank" href="https://codecore.ca/"><span> CodeCore Developer Bootcamp</span></a> and the
-            <a target="_blank" href="https://www.uvic.ca/"><span> University of Victoria</span></a>.
+            I am a graduate of the<a target="_blank" rel="noopener noreferrer" href="https://codecore.ca/"><span> CodeCore Developer Bootcamp</span></a>,
+            the <a target="_blank" href="https://www.uvic.ca/" rel="noopener noreferrer"><span> University of Victoria</span></a> and <a target="_blank" href="http://camosun.ca/" rel="noopener noreferrer"><span> Camosun College</span></a>.
           </h3>
 
           <h3>
@@ -30,7 +30,14 @@ class About extends Component {
           </h3>
 
           <h3>
-            As an undergraduate in Uvic's psychology program, I enrolled in computer science electives, and fell in love immediately. So much so, I decided to enroll in CodeCore and change career paths.
+            As an undergraduate in Uvic's psychology program, I enrolled in computer science electives,
+            and fell in love immediately. So much so, I decided to enroll in CodeCore and change career paths.
+          </h3>
+
+          <h3>
+            At Camosun College, in the Arts and Sciences department, I took several courses including:
+            animation, art theory, drawing, filmmaking, printmaking, painting, graphic design, ceramics, sculpture, 3D design, video,
+            modern art history, and photography.
           </h3>
 
           <h3>
